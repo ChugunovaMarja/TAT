5ccc7738601d0f30d70754932d331e16fadb16c2
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-03 00:58:06 +0400

    changed lines

--- CommandLineParams.java
+++ CommandLineParams.java
@@ -7,14 +7,9 @@ public class CommandLineParams {
 * displays lines in reverse order
 */
 public static void main(String[] args) { 
-
-for(int i = args.length-1; i >= 0 ; i--) {
-        
-            System.out.println("Argument " + i + " = " 
-
-+ args[i]);
-        }
-
+    for(int i = args.length-1; i >= 0 ; i--) {
+        System.out.println("Argument " + i + " = " + args[i]);
+    }
 }
 }
 
