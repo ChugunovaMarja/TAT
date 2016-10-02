dc0a5d7ed5d7fa3f9317f1692c11c9ec09286b3c
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-03 00:24:46 +0400

    spaces

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
-/**main class for calculating sum, 
+/** main class for calculating sum, 
 * difference, cup product and 
-*quotient of two numbers
+* quotient of two numbers
 */
 public class Calculator {
 /**
@@ -12,26 +12,24 @@ double a = Double.parseDouble(args[0]);
 double b = Double.parseDouble(args[1]);
 
 if (b != 0) {
-   System.out.println("Sum = " + sum(a,b));
-   System.out.println("Difference = " + dif(a,b));
-   System.out.println("Cup Product = " + cupProduct(a,b)); 
-   System.out.println("Quotient = " + quotient(a,b));
- }
- else
-   System.out.println("Cannot divide by zero");
+    System.out.println("Sum = " + sum(a,b));
+    System.out.println("Difference = " + dif(a,b));
+    System.out.println("Cup Product = " + cupProduct(a,b)); 
+    System.out.println("Quotient = " + quotient(a,b));
+} else
+    System.out.println("Cannot divide by zero");
 }
 
 public static double sum(double a, double b) { //calculates sum of two numbers
-   return (a+b);
+    return (a+b);
 }
 public static double dif(double a, double b) { //calculates difference of two numbers
-   return (a-b);
+    return (a-b);
 }
 public static double cupProduct(double a, double b) { //calculates cup product of two numbers
-   return (a*b);
+    return (a*b);
 } 
 public static double quotient(double a, double b) { //calculates quotient of two numbers
-   return (a/b);
+    return (a/b);
 }
-
 }
