aaf2f5ddbaab9f1bfa404f0c2a1b95c00aa47773
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-03 17:29:48 +0400

    moved asterisks

--- PrintHelloName.java
+++ PrintHelloName.java
@@ -1,16 +1,15 @@
 /** 
-* Main class 
-* for printing "Hello, [Name]"
-*/
+ * Main class 
+ * for printing "Hello, [Name]"
+ */
 public class PrintHelloName {
 /** 
-* Prints "Hello, [Name]"
-*/
+ * Prints "Hello, [Name]"
+ */
 public static void main(String[] args) { 
     if (args.length > 0) {
         System.out.println("Hello, " + args[0]);
-    }
-    else {
+    } else {
         System.out.println("No arguments");
     }
 }
