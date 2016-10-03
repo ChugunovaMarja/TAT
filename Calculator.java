845317cdf2adba77fa639ad4ee69f538c448b890
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-03 18:15:24 +0400

    zeros

--- Calculator.java
+++ Calculator.java
@@ -12,14 +12,20 @@ public static void main(String[] args) {
 double a = Double.parseDouble(args[0]);
 double b = Double.parseDouble(args[1]);
 
-if (b != 0.0) {
-    System.out.println("Sum = " + sum(a,b));
-    System.out.println("Difference = " + dif(a,b));
-    System.out.println("Cup Product = " + cupProduct(a,b)); 
-    System.out.println("Quotient = " + quotient(a,b));
-} else {
-    System.out.println("Cannot divide by zero");
-  }
+if (Double.isInfinite(a/b)) {
+    System.out.println("Division by zero");
+    System.exit(0);
+    }
+if (Double.isNaN(a/b)){
+    System.out.println("Division zero by zero");
+    System.exit(0);
+    }
+
+System.out.println("Sum = " + sum(a,b));
+System.out.println("Difference = " + dif(a,b));
+System.out.println("Cup Product = " + cupProduct(a,b)); 
+System.out.println("Quotient = " + quotient(a,b));
+
 }
 /**
  * calculates sum of two numbers
