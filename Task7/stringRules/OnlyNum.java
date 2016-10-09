136a0a14f4e7936cd07ad4ba76a61ee10dec6405
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-09 18:39:03 +0400

    Create OnlyNum.java

---
+++ Task7/stringRules/OnlyNum.java
@@ -0,0 +1,29 @@
+package stringRules;
+
+/**
+ * extended from Rule class that 
+ * specifies rule №2 "Contains only numbers"
+ * @param line is an original line that must be
+ * checked to compliance to described rule
+ */
+public class OnlyNum extends Rule {
+    
+    String line;
+    
+    public OnlyNum(String line) {
+        this.line = line;
+    }
+    
+    /**
+     * checks if line has only numbers
+     * @return string message about compliance if it does
+     */
+    @Override
+    public String checkRule() {
+        if (line.matches("^\\d+$")) {
+            return ("Complies rule 'Has only numbers' \n");
+        } else {
+            return null;
+        }
+    }
+}
