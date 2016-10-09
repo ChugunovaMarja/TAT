660ee740f34dfd68f3c80f348c6994c9595f1301
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-10-09 18:33:10 +0400

    Create StringRules.java

---
+++ Task7/stringRules/StringRules.java
@@ -0,0 +1,48 @@
+package stringRules;
+
+import java.util.Scanner;
+
+/**
+ * Main class, contains point of entrance
+ * @author Maria
+ */
+public class StringRules {
+
+    /**
+     * point of entrance,
+     * asks for string line and creates 
+     * instances of the rule classes
+     * 
+     * @param line is a string that will be checked
+     * for compliance to rules;
+     * @param args is ignored;
+     */
+    public static void main(String[] args) {
+        
+        try{
+            Scanner sc = new Scanner(System.in);
+          System.out.println("Print line");
+          String line = sc.nextLine();
+        
+          NoNum noNum = new NoNum(line);
+          printResults(noNum);
+          OnlyNum onlyNum = new OnlyNum(line);
+          printResults(onlyNum);
+          MoreThanFiveWords moreThanFiveWords = new MoreThanFiveWords(line);
+          printResults(moreThanFiveWords);
+          DictionaryWord dictionaryWord = new DictionaryWord(line);
+          printResults(dictionaryWord);
+        } catch (Exception e) {
+            System.out.println("Something went wrong, restart the program");
+        }
+    }
+    
+    /**
+     * prints the results of overloaded method checkRule, 
+     * that checks if line complies to rule
+     * @param rule is an instance of specific rule
+     */
+    public static void printResults(Rule rule) {
+      System.out.println(rule.checkRule());    
+    }
+}
