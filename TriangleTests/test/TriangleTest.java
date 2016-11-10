042cc36bbf105fd829af84231368bc738d0b40b1
ChugunovaMarja <chuhunovamarja@gmail.com>
2016-11-10 17:38:01 +0400

    triangle test changed

--- TriangleTests/test/TriangleTest.java
+++ TriangleTests/test/TriangleTest.java
@@ -9,35 +9,54 @@ import static org.testng.Assert.*;
  * Created by Maria on 07.11.2016.
  */
 public class TriangleTest {
+    private static final String INVALID_SIDES = "invalidSides";
+    private static final String SIDE_A = "side_a";
+    private static final String SIDE_B = "side_b";
+    private static final String SIDE_C = "side_c";
 
-    @DataProvider(name = "invalid triangle sides")
-    public Object[][] getSides() {
-        return new Object[][]{
-                {-1, 2, 3},
-                {1, -2, 3},
-                {1, 2, -3},
-                {0, 1, 2},
-                {1, 0, 2},
-                {1, 2, 0},
-                {Double.MAX_VALUE, 2, 3},
-                {1, Double.MAX_VALUE, 3},
-                {1, 2, Double.MAX_VALUE},
-                {Double.NaN, 2, 3},
-                {1, Double.NaN, 3},
-                {1, 2, Double.NaN},
-                {Double.POSITIVE_INFINITY, 2, 3},
-                {1, Double.POSITIVE_INFINITY, 3},
-                {1, 2, Double.POSITIVE_INFINITY},
-                {Double.NEGATIVE_INFINITY, 2, 3},
-                {1, Double.NEGATIVE_INFINITY, 3},
-                {1, 2, Double.NEGATIVE_INFINITY},
-                {1, 1, 10}
-        };
+    public Document getDocument() throws ParserConfigurationException, IOException, SAXException {
+        File inputFile = new File("./data.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(inputFile);
+
+        return document;
     }
 
-    @Test (dataProvider = "invalid triangle sides")
+    public double getValue(String stringValue) {
+
+        if(stringValue.equals("Double.MinValue")) {
+            return Double.MIN_VALUE;
+        }
+        if(stringValue.equals("Double.MaxValue")) {
+            return Double.MAX_VALUE;
+        }
+        return Double.parseDouble(stringValue);
+    }
+
+    @DataProvider(name = "invalid sides")
+    public Object[][] getData() throws Exception {
+
+        Document document = getDocument();
+        NodeList nodes = document.getElementsByTagName(INVALID_SIDES);
+        Object [][] result = new Double[nodes.getLength()][];
+
+        for(int i = 0; i < nodes.getLength(); i++) {
+            NamedNodeMap attrs = nodes.item(i).getAttributes();
+            result[i] = new Double[] {
+                    getValue(attrs.getNamedItem(SIDE_A).getNodeValue()),
+                    getValue(attrs.getNamedItem(SIDE_B).getNodeValue()),
+                    getValue(attrs.getNamedItem(SIDE_C).getNodeValue())
+            };
+        }
+        return result;
+    }
+
+
+    @Test(dataProvider = "invalid sides")
     public void testGetTypeNegative(double a, double b, double c) throws Exception {
-        Triangle triangle = new Triangle(a,b,c);
+        Triangle triangle = new Triangle(a, b, c);
         assertTrue(triangle.getType().equals("Triangle doesnt exist"));
     }
 
